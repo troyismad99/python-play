769ed9633272d6cea583a9e5f515993df41ea1ed
troyismad99 <troyhamilton@gmail.com>
2021-04-01 10:38:26 -0700

    Decompress Run-Length Encoded List

---
+++ leetcode/1313-DecompressRunLengthEncodedList.py
@@ -0,0 +1,52 @@
+'''
+1313. Decompress Run-Length Encoded List
+
+We are given a list nums of integers representing a list compressed with
+run-length encoding.
+
+Consider each adjacent pair of elements 
+[freq, val] = [nums[2*i], nums[2*i+1]] (with i >= 0).
+
+For each such pair, there are freq elements with value val concatenated in
+a sublist. Concatenate all the sublists from left to right to generate 
+the decompressed list.
+
+Return the decompressed list.
+
+Example 1:
+    Input: nums = [1,2,3,4]
+    Output: [2,4,4,4]
+    Explanation: 
+    - The first pair [1,2] means we have freq = 1 and val = 2 so 
+      we generate the array [2].
+    - The second pair [3,4] means we have freq = 3 and val = 4 so 
+      we generate [4,4,4].
+    - At the end the concatenation [2] + [4,4,4] is [2,4,4,4].
+
+Example 2:
+    Input: nums = [1,1,2,3]
+    Output: [1,3,3]
+
+Constraints:
+    2 <= nums.length <= 100
+    nums.length % 2 == 0
+    1 <= nums[i] <= 100
+'''
+# Runtime: 64 ms,      faster than 79.07% of Python3 online submissions for Decompress Run-Length Encoded List.
+# Memory Usage: 14.9 MB, less than 28.34% of Python3 online submissions for Decompress Run-Length Encoded List.
+
+class Solution:
+    def decompressRLElist(self, nums: list[int]) -> list[int]:
+        result = []
+
+        for i in range(0, len(nums), 2):
+            result += [nums[i+1]] * nums[i]
+
+        return result
+
+# examples
+s = Solution()
+
+print(s.decompressRLElist([1,2,3,4]))
+print(s.decompressRLElist([1,1,2,3]))
+print(s.decompressRLElist([99,99]))
